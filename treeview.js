4e59158617b4c3513f8b56dad35a711937092294
fturturo <36268583+fturturo@users.noreply.github.com>
2022-01-13 18:45:44 +0100

    Add tree view example with CSS and JS

---
+++ treeview.js
@@ -0,0 +1,11 @@
+function expandableTree() {
+	var toggler = document.getElementsByClassName("sezione");
+	var i;
+	
+	for (i = 0; i < toggler.length; i++) {
+	  toggler[i].addEventListener("click", function() {
+	    this.parentElement.querySelector(".nested").classList.toggle("active");
+	    this.classList.toggle("sezione-expanded");
+	  });
+	}
+}
